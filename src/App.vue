be0f62ddcdcee75d68e8524678f6c98b10481f3a
EliaBuratti <elia.buratti.dev@gmail.com>
2023-09-21 15:39:18 +0200

    add option object on vue app

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,39 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  // questo é il vs options object
+  data() {
+    return {
+      message: 'Hello Vite',
+    }
+  },
+  methods: {
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>{{ message }}</h1>
 </template>
+
+
+
+
+
+
+
+
+
+<!-- <script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script> -->
+
+<!-- <template>
+  <div>
+    <h1>hello vue</h1>
+  </div>
+    <HelloWorld msg="Vite + Vue" />
+
+</template> -->
 
 <style scoped>
 .logo {
@@ -21,9 +42,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
