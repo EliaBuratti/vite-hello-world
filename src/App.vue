ae45923343fe81479358a427a2b555eb04506ef7
EliaBuratti <elia.buratti.dev@gmail.com>
2023-09-21 16:41:10 +0200

    add jumbotron in AppHero

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script>
 import AppTitle from './components/AppTitle.vue'
+import AppHero from './components/AppHero.vue'
 
 export default {
   components: {
-    AppTitle
+    AppTitle,
+    AppHero
   },
   // questo é il vs options object
   data() {
@@ -19,6 +21,8 @@ export default {
 </script>
 
 <template>
+  <AppHero />
+
   <!-- with option object in the same app -->
   <h1>{{ message }}</h1>
 
@@ -26,39 +30,4 @@ export default {
   <AppTitle />
 </template>
 
-
-
-
-
-
-
-
-
-<!-- <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script> -->
-
-<!-- <template>
-  <div>
-    <h1>hello vue</h1>
-  </div>
-    <HelloWorld msg="Vite + Vue" />
-
-</template> -->
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+    // questo é il vs options object
+    data() {
+        return {
+            message: 'Benvenuto nel fantastico mondo di Vite + Vue',
+        }
+    },
+    methods: {
+    }
+}
+</script>
+
+<template>
+    <div class="jumbo">
+        <div class="title">
+            <h1>{{ message }}</h1>
+        </div>
+        <div class="description">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum obcaecati officia voluptas velit maiores
+                molestiae vel sunt facere sint mollitia laudantium magni quisquam odio fuga quasi, a eveniet perspiciatis
+                dolores.
+            </p>
+            <button class="logo">Entra ora</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.jumbo {
+    background-color: rgb(83, 94, 104);
+    padding: 1rem;
+    border-radius: 2rem;
+}
+
+.title {
+    border-bottom: 1px solid rgba(255, 255, 255, 0.5);
+}
+
+button {
+    transition: all 0.7s;
+}
+
+button:hover {
+
+    transform: scale(1.5);
+}
+</style>
